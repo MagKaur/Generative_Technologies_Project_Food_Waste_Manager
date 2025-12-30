ea775eb719a550d0f3f85a8a3089648f5ce5c936
MagKaur <MagKaur@example.com>
2025-12-30 01:53:37 +0100

    *Changed hardcoded password for variable in docker-compose.yaml command *added warning "DB_PASSWORD" is not set to docker-compose.yaml command *deleted unnecessary fallbacks in config.py *added comment with instruction for .env file to .env.example

--- src/config/config.py
+++ src/config/config.py
@@ -1,22 +1,25 @@
 import os
 import logging
+from pathlib import Path
+
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='C:\\Users\\dawid\\PycharmProjects\\Generative_Technologies_Project_Food_Waste_Manager\\src\\config\\.env')
+load_dotenv(Path(__file__).resolve().parent / ".env")
+# load_dotenv(dotenv_path='C:\\Users\\dawid\\PycharmProjects\\Generative_Technologies_Project_Food_Waste_Manager\\src\\config\\.env')
 
 DB_USER = os.getenv("DB_USER", "neo4j")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "neo4j")
-DB_URI = os.getenv("DB_URI","")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_URI = os.getenv("DB_URI")
 DB_DATABASE = os.getenv("DB_DATABASE", "neo4j")
 
-RAW_DATASET_PATH = os.getenv("RAW_DATASET_PATH", "")
-DATASET_PATH = os.getenv("DATASET_PATH", "")
+RAW_DATASET_PATH = os.getenv("RAW_DATASET_PATH")
+DATASET_PATH = os.getenv("DATASET_PATH")
 
-AZURE_OPENAI_MODEL = os.getenv("AZURE_OPENAI_MODEL", "gpt-4.1-nano")
-AZURE_OPENAI_EMBEDDING_MODEL = os.getenv("AZURE_OPENAI_EMBEDDING_MODEL", "text-embedding-3-small")
+AZURE_OPENAI_MODEL = os.getenv("AZURE_OPENAI_MODEL")
+AZURE_OPENAI_EMBEDDING_MODEL = os.getenv("AZURE_OPENAI_EMBEDDING_MODEL")
 AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
 AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION", "2024-12-01-preview")
+OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION")
 
 
 
